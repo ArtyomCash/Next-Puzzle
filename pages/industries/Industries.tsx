b4e7b84aad81cda0d4ce691aafe9b908f5e3f6e3
Artyom <cash.kin.art@gmail.com>
2022-07-19 23:51:44 +0300

    desktop position absolute

--- pages/industries/Industries.tsx
+++ pages/industries/Industries.tsx
@@ -23,282 +23,175 @@ import Image from 'next/image';
 const puzzleArray = [
   { namePlayer: 'Семён', idPlayer: 1, password: '1' },
   {
-    storagePuzzle: {
-      idStoragePuzzle: 1,
-      assembled: true,
-      arrayStoragePuzzle: [
-        {
-          idPuzzlePiece: 0,
-          imgPuzzlePiece: wood_01.src,
-        },
-        {
-          idPuzzlePiece: 1,
-          imgPuzzlePiece: wood_02.src,
-        },
-        {
-          idPuzzlePiece: 2,
-          imgPuzzlePiece: wood_03.src,
-        },
-        {
-          idPuzzlePiece: 3,
-          imgPuzzlePiece: wood_04.src,
-        },
-        {
-          idPuzzlePiece: 4,
-          imgPuzzlePiece: wood_05.src,
-        },
-        {
-          idPuzzlePiece: 5,
-          imgPuzzlePiece: wood_06.src,
-        },
-        {
-          idPuzzlePiece: 6,
-          imgPuzzlePiece: wood_07.src,
-        },
-        {
-          idPuzzlePiece: 7,
-          imgPuzzlePiece: wood_08.src,
-        },
-        {
-          idPuzzlePiece: 8,
-          imgPuzzlePiece: wood_09.src,
-        },
-        {
-          idPuzzlePiece: 9,
-          imgPuzzlePiece: wood_10.src,
-        },
-        {
-          idPuzzlePiece: 10,
-          imgPuzzlePiece: wood_11.src,
-        },
-        {
-          idPuzzlePiece: 11,
-          imgPuzzlePiece: wood_12.src,
-        },
-        {
-          idPuzzlePiece: 12,
-          imgPuzzlePiece: wood_13.src,
-        },
-        {
-          idPuzzlePiece: 13,
-          imgPuzzlePiece: wood_14.src,
-        },
-        {
-          idPuzzlePiece: 14,
-          imgPuzzlePiece: wood_15.src,
-        },
-        {
-          idPuzzlePiece: 15,
-          imgPuzzlePiece: wood_16.src,
-        },
-      ],
-      collectedElements: [
-        {
-          imgPuzzlePiece: '',
-        },
-        {
-          imgPuzzlePiece: '',
-        },
-        {
-          imgPuzzlePiece: '',
-        },
-        {
-          imgPuzzlePiece: '',
-        },
-        {
-          imgPuzzlePiece: '',
-        },
-        {
-          imgPuzzlePiece: '',
-        },
-        {
-          imgPuzzlePiece: '',
-        },
-        {
-          imgPuzzlePiece: '',
-        },
-        {
-          imgPuzzlePiece: '',
-        },
-        {
-          imgPuzzlePiece: '',
-        },
-        {
-          imgPuzzlePiece: '',
-        },
-        {
-          imgPuzzlePiece: '',
-        },
-        {
-          imgPuzzlePiece: '',
-        },
-        {
-          imgPuzzlePiece: '',
-        },
-        {
-          imgPuzzlePiece: '',
-        },
-        {
-          imgPuzzlePiece: '',
-        },
-      ],
-    },
+    storagePuzzle: [
+      {
+        idStoragePuzzle: 1,
+        assembled: false,
+        puzzleName: 'One Wood',
+        reward: false,
+        arrayStoragePuzzleWood: [
+          {
+            idPuzzlePiece: 0,
+            imgPuzzlePiece: wood_01.src,
+          },
+          {
+            idPuzzlePiece: 1,
+            imgPuzzlePiece: wood_02.src,
+          },
+          {
+            idPuzzlePiece: 2,
+            imgPuzzlePiece: wood_03.src,
+          },
+          {
+            idPuzzlePiece: 3,
+            imgPuzzlePiece: wood_04.src,
+          },
+          {
+            idPuzzlePiece: 4,
+            imgPuzzlePiece: wood_05.src,
+          },
+          {
+            idPuzzlePiece: 5,
+            imgPuzzlePiece: wood_06.src,
+          },
+          {
+            idPuzzlePiece: 6,
+            imgPuzzlePiece: wood_07.src,
+          },
+          {
+            idPuzzlePiece: 7,
+            imgPuzzlePiece: wood_08.src,
+          },
+          {
+            idPuzzlePiece: 8,
+            imgPuzzlePiece: wood_09.src,
+          },
+          {
+            idPuzzlePiece: 9,
+            imgPuzzlePiece: wood_10.src,
+          },
+          {
+            idPuzzlePiece: 10,
+            imgPuzzlePiece: wood_11.src,
+          },
+          {
+            idPuzzlePiece: 11,
+            imgPuzzlePiece: wood_12.src,
+          },
+          {
+            idPuzzlePiece: 12,
+            imgPuzzlePiece: wood_13.src,
+          },
+          {
+            idPuzzlePiece: 13,
+            imgPuzzlePiece: wood_14.src,
+          },
+          {
+            idPuzzlePiece: 14,
+            imgPuzzlePiece: wood_15.src,
+          },
+          {
+            idPuzzlePiece: 15,
+            imgPuzzlePiece: wood_16.src,
+          },
+        ],
+        collectedElements: [
+          {
+            imgPuzzlePiece: '',
+          },
+          {
+            imgPuzzlePiece: '',
+          },
+          {
+            imgPuzzlePiece: '',
+          },
+          {
+            imgPuzzlePiece: '',
+          },
+          {
+            imgPuzzlePiece: '',
+          },
+          {
+            imgPuzzlePiece: '',
+          },
+          {
+            imgPuzzlePiece: '',
+          },
+          {
+            imgPuzzlePiece: '',
+          },
+          {
+            imgPuzzlePiece: '',
+          },
+          {
+            imgPuzzlePiece: '',
+          },
+          {
+            imgPuzzlePiece: '',
+          },
+          {
+            imgPuzzlePiece: '',
+          },
+          {
+            imgPuzzlePiece: '',
+          },
+          {
+            imgPuzzlePiece: '',
+          },
+          {
+            imgPuzzlePiece: '',
+          },
+          {
+            imgPuzzlePiece: '',
+          },
+        ],
+      },
+    ],
   },
 ];
 
-const arrayImg = puzzleArray[1].storagePuzzle?.arrayStoragePuzzle;
-const collected = puzzleArray[1].storagePuzzle?.collectedElements;
-
-/*const replacementPieces = () => {
-  arrayImg?.forEach((puzzle) => {
-    console.log(puzzle);
-    const currentIndex = arrayImg.indexOf(puzzle);
-    console.log('currentIndex>>>>>', currentIndex);
-    arrayImg.splice(currentIndex, 0);
-  });
-};
-replacementPieces();*/
-
-// console.log('puzzleArray>>>', puzzleArray[1].storagePuzzle);
+// console.log('puzzleArray', puzzleArray[1]?.storagePuzzle[0].arrayStoragePuzzleWood[0].imgPuzzlePiece);
 
 const Industries: NextPage = () => {
-  const [currentPuzzle, setCurrentPuzzle] = useState(puzzleArray);
-  console.log('сейчас в useSate', currentPuzzle);
-  const dragOverHandler = (e: any) => {
-    e.preventDefault();
-    // console.log('dragOverHandler');
-  };
-  const dragLeaveHandler = (e: any) => {};
-  const dragStartHandler = (e: any, puzzle: any) => {
-    // то элемент который взял, записываем в useState
-    setCurrentPuzzle(puzzle);
-    // удаляем этот элемент из массива
-    // const currentIndex = arrayImg?.indexOf(puzzle);
-    // setInterval(() => arrayImg?.splice(puzzle, 1), 100);
-    console.log('Элемент который взял', puzzle);
-    arrayImg?.splice(puzzle, 1);
-  };
-  const dragStartHandlerDesktop = (e: any, puzzleDesktop: any) => {
-    // const smotry = collected?.splice(puzzleDesktop, 1);
-    console.log('забрал с десктопа');
-  };
-
-  const dragEndHandler = (e: any) => {
-    e.target.style.boxShadow = 'none';
-  };
-  const dropHandler = (e: any, puzzle: any) => {
-    // на какой элемент кидаешь пазл
-    // console.log('e', e.dataTransfer);
-    // console.log('111111');
-    e.preventDefault();
-    console.log('на какой элемент кидаешь пазл', puzzle);
-  };
-
-  const dragPuzzleOverHandler = (e: any, puzzleDesktop: any) => {
-    // добавляем новый кусочек пазла
-    console.log('добавляем новый кусочек пазла');
-    console.log('puzzleDesktop', puzzleDesktop);
-    const dobavil = collected?.push(puzzleDesktop);
-    console.log('dobavil>>>', dobavil);
-  };
-
-  const dropPuzzleHandler = (e: any, puzzleDesktop: any) => {
-    console.log('puzzleDesktop', puzzleDesktop);
-    const puzzleDesktopIndex = collected?.indexOf(puzzleDesktop);
-    // puzzleDesktop.push(currentPuzzle);
-    console.log('puzzleDesktopIndex', puzzleDesktopIndex);
-  };
-
   return (
-    <section
-      className={styles.array}
-      onDragOver={(e) => dragOverHandler(e)}
-      // onDrop={(e) => dropHandler(e)}
-      // onDragLeave={(e) => dragLeaveHandler(e)}
-      // onDragStart={(e) => dragStartHandler(e)}
-      // onDragEnd={(e) => dragEndHandler(e)}
-      draggable={true}>
-      {/*<div className={styles.storagePuzzle}>
-        {puzzleArray[1].storagePuzzle?.arrayStoragePuzzle.map((puzzle, puzzleIndex) => (
-          <div
-            className={styles.imgPuzzle}
-            key={puzzleIndex}
-            onDragOver={(e) => dragOverHandler(e)}
-            onDragLeave={(e) => dragLeaveHandler(e)}
-            onDragStart={(e) => dragStartHandler(e, puzzle)}
-            onDragEnd={(e) => dragEndHandler(e)}
-            onDrop={(e) => dropHandler(e, puzzle)}
-            draggable={true}>
-            <Image width={200} height={200} src={puzzle.imgPuzzlePiece} alt='' />
-          </div>
-        ))}
-      </div>
-      <div className={styles.desktop}>
-        {puzzleArray[1].storagePuzzle?.collectedElements.map((puzzle, puzzleIndex) => (
-          <div key={puzzleIndex}>
-            {puzzle.imgPuzzlePiece === '' ? (
-              <div className={styles.imgPuzzle} />
-            ) : (
+    <div>
+      <section
+        className={styles.array}
+        // onDragOver={(e) => dragOverHandler(e)}
+        // onDrop={(e) => dropHandler(e)}
+        // onDragLeave={(e) => dragLeaveHandler(e)}
+        // onDragStart={(e) => dragStartHandler(e)}
+        // onDragEnd={(e) => dragEndHandler(e)}
+        draggable={true}>
+        {puzzleArray[1]?.storagePuzzle?.map((storagePuzzleItem, storagePuzzleIndex) => (
+          <div className={styles.puzzleContainer} key={`storagePuzzleIndex_${storagePuzzleIndex}`}>
+            {storagePuzzleItem.arrayStoragePuzzleWood.map((woodItem, woodIndex) => (
               <div
                 className={styles.imgPuzzle}
-                key={puzzleIndex}
-                onDragOver={(e) => dragOverHandler(e)}
-                // onDragLeave={(e) => dragLeaveHandler(e)}
+                key={`woodIndex_${woodIndex}`}
                 // onDragStart={(e) => dragStartHandler(e, puzzle)}
-                // onDragEnd={(e) => dragEndHandler(e)}
-                onDrop={(e) => dropPuzzleHandler(e, puzzle)}
+                // onDragOver={(e) => dragOverHandler(e)}
+                // onDrop={(e) => dropHandler(e, puzzle)}
                 draggable={true}>
-                <Image width={200} height={200} src={puzzle.imgPuzzlePiece} alt='' />
+                <Image width={200} height={200} src={woodItem.imgPuzzlePiece} alt='' />
               </div>
-            )}
+            ))}
+            <div className={styles.desktop}>
+              {storagePuzzleItem.collectedElements.map((collectedItem, collectedIndex) => (
+                <div
+                  className={styles.imgPuzzle}
+                  key={`collectedIndex_${collectedIndex}`}
+                  // onDragStart={(e) => dragStartHandler(e, puzzle)}
+                  // onDragOver={(e) => dragOverHandler(e)}
+                  // onDrop={(e) => dropHandler(e, puzzle)}
+                  draggable={true}
+                />
+              ))}
+            </div>
           </div>
         ))}
-      </div>*/}
-
-      <div className={styles.storagePuzzle}>
-        {puzzleArray[1].storagePuzzle?.arrayStoragePuzzle.map((puzzle, puzzleIndex) => (
-          <div
-            className={styles.imgPuzzle}
-            key={puzzleIndex}
-            onDragStart={(e) => dragStartHandler(e, puzzle)}
-            onDragOver={(e) => dragOverHandler(e)}
-            onDrop={(e) => dropHandler(e, puzzle)}
-            draggable={true}>
-            <Image width={200} height={200} src={puzzle.imgPuzzlePiece} alt='' />
-          </div>
-        ))}
-      </div>
-      <div className={styles.desktop}>
-        {puzzleArray[1].storagePuzzle?.collectedElements.map((puzzleDesktop, puzzleIndex) => (
-          <div key={puzzleIndex}>
-            {puzzleDesktop.imgPuzzlePiece === '' ? (
-              <div
-                className={styles.imgPuzzle}
-                draggable={true}
-                // onDragOver={(e) => dragPuzzleOverHandler(e, puzzle)}
-                // onDragLeave={(e) => dragLeaveHandler(e)}
-                onDragStart={(e) => dragStartHandlerDesktop(e, puzzleDesktop)}
-                // onDragEnd={(e) => dragEndHandler(e)}
-                onDrop={(e) => dropPuzzleHandler(e, puzzleDesktop)}
-              />
-            ) : (
-              <div
-                className={styles.imgPuzzle}
-                key={puzzleIndex}
-                onDragOver={(e) => dragPuzzleOverHandler(e, puzzleDesktop)}
-                onDragLeave={(e) => dragLeaveHandler(e)}
-                onDragStart={(e) => dragStartHandler(e, puzzleDesktop)}
-                onDragEnd={(e) => dragEndHandler(e)}
-                // onDrop={(e) => dropPuzzleHandler(e, puzzle)}
-                onDrop={(e) => dropPuzzleHandler(e, puzzleDesktop)}
-                draggable={true}>
-                <Image width={200} height={200} src={puzzleDesktop.imgPuzzlePiece} alt='' />
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
